c8d935f231af702a4229ec6cb109182f2779d66c
yaroslavartist <34551737+yaroslavartist@users.noreply.github.com>
2021-12-16 13:15:55 +0200

    Add files via upload

---
+++ docs/script/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+        $(".item").magnificPopup({
+        	type : 'image',
+        	fixedContentPos: true,
+        	closeBtnInside: false,
+		fixedContentPos: true,
+        	removalDelay: 300,
+        	mainClass: 'mfp-fade',
+        });
+    });
+
+let scroll = document.getElementById('header-scroll');
+
